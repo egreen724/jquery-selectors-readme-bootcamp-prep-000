5bb9c35ea01dbc0a49872ecd2a4c12dbc27918a8
Erin Green <erin.y.green@gmail.com>
2018-07-27 21:43:23 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,2 +1,21 @@
 // declare your functions here...
-$('.pics')
+
+function paragraphSelector() {
+  return $('p');
+}
+
+function lastImageSelector() {
+  return $('img:last');
+}
+
+function ninjaBabySelector(){
+  return $('#babyninja')
+}
+
+function divSelector() {
+  return $('.pics')
+}
+
+function firstListItem() {
+  return $('ul li:first-child')
+}
